a9934dd6c1e1d026e6cf8a225bdd0f9f9d83045a
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-11-11 20:58:17 -0500

    feat: start caching user before ban so appeals load correctly

--- events/on_ban.js
+++ events/on_ban.js
@@ -2,6 +2,8 @@ const { Events, MessageFlags } = require('discord.js')
 module.exports = {
     name: Events.GuildBanAdd,
     async execute(ban) {
-
+        let user;
+        user = ban.user
+        updateCache
     }
 }
