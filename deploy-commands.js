7cb36479fa86336bb86d584892eaa178e403c6e6
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-12-06 00:09:36 -0500

    refactor: simplify configuration loading and standardize file system imports

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,23 +1,27 @@
 const { REST, Routes } = require('discord.js');
-const clientId = require('/home/ken/.dbab-config/dbab.json').bot.clientId;
-const guildIds = require('/home/ken/.dbab-config/dbab.json').bot.guildIds;
-const token = require('/home/ken/.dbab-config/dbab.json').bot.token;
-const fs = require('node:fs');
-const path = require('node:path');
+const cfg = require('/home/ken/.dbab-config/dbab.json');
+const clientId = cfg.bot.clientId;
+const guildIds = cfg.bot.guildIds;
+const token = cfg.bot.token;
+console.log(clientId)
+console.log(token)
+const { readdirSync } = require('node:fs');
+const { join } = require('node:path');
+//const __dirname =  import.meta.dirname;
 
 const commands = [];
 console.log(guildIds)
 // Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const foldersPath = join(__dirname, 'commands');
+const commandFolders = readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
     // Grab all the command files from the commands directory you created earlier
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const commandsPath = join(foldersPath, folder);
+    const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
     // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
     for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
+        const filePath = join(commandsPath, file);
         const command = require(filePath);
         if ('data' in command && 'execute' in command) {
             commands.push(command.data.toJSON());
